e0c29c1a4a96ed16d8d87e617542cb3e3f59f42e
analistaesportivobet365 <analistaesportivobet365@gmail.com>
2026-02-16 08:17:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const DB_FILE = 'database.json';
 
 if (!fs.existsSync(DB_FILE)) {
